01c5d0b19d6b8eaac54569faf79c18d54aff9b22
billy0705 <billyg123@gmail.com>
2024-05-28 15:54:32 +0200

    add group 4

---
+++ group_4/aggregation.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import time
+
+
+def validate(actual_result):
+    expected_result = pd.read_csv('dmv_fuel_type_passengers_expected.csv')
+    if not actual_result.equals(expected_result):
+        print("EXPECTED:\n===")
+        print(expected_result)
+        print("===\nACTUAL:\n===")
+        print(actual_result)
+        print("===")
+        return False
+    return True
+
+
+def query(df):
+    # TODO: Implement the query and return a data frame with the result.
+    # select fuel_type,
+    #        count(*) as vehicle_count,
+    #        round(avg(passengers), 1) as avg_passengers
+    # from dmv group by fuel_type
+    # order by fuel_type;
+    count_dict = {}
+    avg_dict = {}
+    print(df.info())
+    for index, row in df.iterrows():
+        print(row["fuel_type"], row["passengers"])
+        if row["fuel_type"] in count_dict:
+            if pd.isna(row["passengers"]):
+                count_dict[row["fuel_type"]] += 1
+                avg_dict[row["fuel_type"]] += int(row["passengers"])
+        else:
+            if pd.isna(row["passengers"]):
+                count_dict[row["fuel_type"]] = 1
+                avg_dict[row["fuel_type"]] = int(row["passengers"])
+                    
+        print()
+
+
+    return pd.DataFrame(columns=['fuel_type', 'vehicle_count', 'avg_passengers'], data=[('X', 0, 0)])
+
+
+# Read data
+df = pd.read_csv('dmv_fuel_type_passengers_sample.csv')
+
+# Run query (data is loaded before, everything else needs to be timed)
+start = time.time()
+result = query(df)
+end = time.time()
+
+# Validate result and print time
+if validate(result):
+    print("Result:", end - start)
+else:
+    print("Result: Error")
